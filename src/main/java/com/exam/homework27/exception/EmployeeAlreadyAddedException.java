7e997efb3cbb8c19e4653130184f45750c9a5eb7
Лилиана <lilya_gazina@mail.ru>
2023-04-16 17:40:18 +0300

    1

--- src/main/java/com/exam/homework27/exception/EmployeeAlreadyAddedException.java
+++
@@ -1,9 +0,0 @@
-package com.exam.homework27.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class EmployeeAlreadyAddedException extends RuntimeException {
-
-}
